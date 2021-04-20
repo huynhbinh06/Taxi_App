223d183777d0ee3f3b7a89d17404e45efdec55ec
huynhbinh06 <huynhxuanbinh2000@gmail.com>
2021-04-20 11:26:38 +0700

    login ui and validators

---
+++ android/app/src/main/kotlin/com/huynhbinh/taxi_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.huynhbinh.taxi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/resources/register_page.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+import 'package:taxi_app/src/blocs/auth_bloc.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  AuthBloc authBloc = new AuthBloc();
+
+  TextEditingController _nameController = new TextEditingController();
+  TextEditingController _phoneController = new TextEditingController();
+  TextEditingController _emailController = new TextEditingController();
+  TextEditingController _passController = new TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    authBloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Color(0xff3277D8)),
+        elevation: 0,
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Image.asset("ic_car_red.png"),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 6),
+                child: Text(
+                  "Welcome Abroad!",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Color(0xff333333),
+                  ),
+                ),
+              ),
+              Text(
+                "Sign up with iCar in simple steps",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color(0xff606467),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 80, 0, 20),
+                child: StreamBuilder(
+                  stream: authBloc.nameStream,
+                  builder: (context, snapshot) => TextField(
+                    controller: _nameController,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      labelText: "Name",
+                      errorText: snapshot.hasError ? snapshot.error : null,
+                      prefixIcon: Container(
+                        width: 50,
+                        child: Image.asset("ic_user.png"),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xffCED0D2),
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(6.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              StreamBuilder(
+                stream: authBloc.phoneStream,
+                builder: (context, snapshot) => TextField(
+                  controller: _phoneController,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    labelText: "Phone Number",
+                    errorText: snapshot.hasError ? snapshot.error : null,
+                    prefixIcon: Container(
+                      width: 50,
+                      child: Image.asset("ic_phone.png"),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Color(0xffCED0D2),
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(6.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                child: StreamBuilder(
+                  stream: authBloc.emailStream,
+                  builder: (context, snapshot) => TextField(
+                    controller: _emailController,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      errorText: snapshot.hasError ? snapshot.error : null,
+                      prefixIcon: Container(
+                        width: 50,
+                        child: Image.asset("ic_mail.png"),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Color(0xffCED0D2),
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(6.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              StreamBuilder(
+                stream: authBloc.passStream,
+                builder: (context, snapshot) => TextField(
+                  obscureText: true,
+                  controller: _passController,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    errorText: snapshot.hasError ? snapshot.error : null,
+                    prefixIcon: Container(
+                      width: 50,
+                      child: Image.asset("ic_lock.png"),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Color(0xffCED0D2),
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(6.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 40),
+                child: SizedBox(
+                  height: 52,
+                  width: double.infinity,
+                  child: RaisedButton(
+                    onPressed: () {
+                      _onSignUpClicked();
+                    },
+                    child: Text(
+                      "Signup",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    color: Color(0xff3277D8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: RichText(
+                  text: TextSpan(
+                      text: "Already a User? ",
+                      style: TextStyle(
+                        color: Color(0xff606470),
+                        fontSize: 16,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "Login now",
+                          style: TextStyle(
+                            color: Color(0xff3277D8),
+                            fontSize: 16,
+                          ),
+                        ),
+                      ]),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _onSignUpClicked() {
+    print(_nameController);
+    var isValid = authBloc.isValid(_nameController.text, _phoneController.text, _emailController.text, _passController.text);
+    if (isValid) {
+      ///Create user
+    }
+  }
+}
